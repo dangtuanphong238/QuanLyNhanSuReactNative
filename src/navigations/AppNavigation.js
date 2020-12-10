6e3c999fa0320564dfcbde9aa94b65e59051c992
dangtuanphong238 <dangtuanphong238@github.com>
2020-12-10 12:59:58 +0700

    Update All

---
+++ src/navigations/AppNavigation.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SignIn from '../screens/Auth/SignIn';
+import SignUp from '../screens/Auth/SignUp';
+import ListRoom from '../screens/Room/ListRoom';
+import Splash from '../screens/Splash/Splash';
+
+const Stack = createStackNavigator();
+
+export default class AppNavigation extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        {/* <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={!this.state.token ? 'SplashScreen' : 'SignInScreen'}> */}
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName={'Splash'}>
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="ListRoom" component={ListRoom} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
